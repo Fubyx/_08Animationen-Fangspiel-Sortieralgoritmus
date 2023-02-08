b1ef6d2d31df681f36c8ad8c0517a3a0395af30f
Fubyx <fabireif11@gmail.com>
2023-02-08 09:19:24 +0100

    resizing of background and maze

--- src/main/java/PacMan/Main.java
+++ src/main/java/PacMan/Main.java
@@ -1,9 +1,6 @@
 package PacMan;
 
 import javafx.application.Application;
-import javafx.beans.binding.Binding;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
@@ -21,7 +18,8 @@ public class Main extends Application {
     final int HEIGHT_WALLNODES = 10;
     Random random = new Random();
 
-    double stageWidth = 700, stageHeight = 700;
+    double sceneWidth, sceneHeight;
+    double backgroundWidth = 700, backgroundHeight = 700;
     Group root = new Group();
     Scene s;
 
@@ -38,7 +36,7 @@ public class Main extends Application {
             }
         }
         generateRandomWallsWithNodes();
-        background = new Rectangle(0, 0, stageWidth, stageHeight);
+        background = new Rectangle(0, 0, backgroundWidth, backgroundHeight);
         background.setFill(Paint.valueOf("black"));
         root.getChildren().add(background);
         buildMaze();
@@ -47,35 +45,42 @@ public class Main extends Application {
         s = new Scene(root);
         primaryStage.setScene(s);
         primaryStage.show();
+        sceneHeight = s.getHeight();
+        sceneWidth = s.getWidth();
 
 
         ChangeListener<Number> resizeListener = new ChangeListener<>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                double oldStageWidth = stageWidth, oldStageHeight = stageHeight;
-                stageWidth = primaryStage.getWidth();
-                stageHeight = primaryStage.getHeight();
+                double oldSceneWidth = sceneWidth, oldSceneHeight = sceneHeight;
+                sceneWidth = s.getWidth();
+                sceneHeight = s.getHeight();
 
                 //background
-                background.setWidth(background.getWidth()/oldStageWidth * stageWidth);
-                background.setHeight(background.getHeight()/oldStageHeight * stageHeight);
-
+                double oldBackgroundHeight = backgroundHeight, oldBackgroundWidth = backgroundWidth;
+                backgroundWidth= backgroundWidth/oldSceneWidth * sceneWidth;
+                backgroundHeight =backgroundHeight/oldSceneHeight * sceneHeight;
+                background.setWidth(backgroundWidth + 1);
+                background.setHeight(backgroundHeight + 1);
                 //maze
                 for (Rectangle i: walls) {
-                    i.setX(i.getX()/oldStageWidth * stageWidth);
-                    i.setY(i.getY());
+                    i.setX(i.getX()/oldBackgroundWidth * backgroundWidth);
+                    i.setY(i.getY()/oldBackgroundHeight * backgroundHeight);
+                    i.setWidth(i.getWidth()/oldBackgroundWidth * backgroundWidth);
+                    i.setHeight(i.getHeight()/oldBackgroundHeight * backgroundHeight);
                 }
             }
         };
         primaryStage.heightProperty().addListener(resizeListener);
+        primaryStage.widthProperty().addListener(resizeListener);
 
     }
 
     public void buildMaze() {
 
-        ArrayList<Rectangle> walls = new ArrayList<>();
-        double sectionWidth = stageWidth / (WIDTH_WALLNODES + 0.5);
-        double sectionHeight = stageHeight / (HEIGHT_WALLNODES + 0.5);
+        walls = new ArrayList<>();
+        double sectionWidth = backgroundWidth / (WIDTH_WALLNODES + 0.5);
+        double sectionHeight = backgroundHeight / (HEIGHT_WALLNODES + 0.5);
 
 
         for (int y = 0; y < wallNodes.size(); ++y) {
